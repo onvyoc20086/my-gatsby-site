1bb242ea85ec73b74e2eb48279c4b6685a9d3c58
gengxu <gengxu@reach.store>
2022-11-09 11:43:02 +0800

    edit

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const About = () => <Layout pageTitle="About"><h1>about</h1></Layout>
+
+export default About
+export const Head = () => <title>About Page</title>
